bfdb249887bcc5eba767c649174cfeb13cf88728
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2025-06-30 11:26:23 -0300

    Upload Exercíse 30/06

---
+++ JS/skeleton.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function logar (e) {
+  e.preventDefault()
+  const username = document.getElementById("username").value
+  let password = document.getElementById("password").value
+
+  const url = "https://dummyjson.com/auth/login"
+  
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  }).then(res => res.json()).then(res => {
+
+    console.log(res)
+  })
+}
